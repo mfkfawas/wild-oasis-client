5afb8c10c6b3d090e1bff7f9fbb4d6ce1a467866
mfkfawas <mfkfawas@gmail.com>
2025-01-15 12:12:46 +0300

    setup: loading and optimizing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import Navigation from "@/app/_components/Navigation";
 
 import "@/app/_styles/global.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export const metadata = {
   // title: "The wild oasis", v1
   title: {
@@ -21,7 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
