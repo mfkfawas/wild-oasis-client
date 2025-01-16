71e4b7b45f39fa55453f163c37761cc482e86f55
mfkfawas <mfkfawas@gmail.com>
2025-01-16 08:25:30 +0300

    feat(ui): optimizing image with Next.js <Image />

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
+
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> v1 */}
+
+      {/* when we import image like this its called statically imported image, and in this scenario we can add some other properties like qualities,... */}
+      <Image
+        src={logo}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+        quality={100} //percentage
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
