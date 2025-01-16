2d1b4e53e27a82bfe7f4a07d92f8066d1d954cce
mfkfawas <mfkfawas@gmail.com>
2025-01-16 03:28:53 +0300

    feat(ui): enhanced Rootlayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "@/app/_components/Header";
 
 import "@/app/_styles/global.css";
 
@@ -29,13 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        {children}
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
