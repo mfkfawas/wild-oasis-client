0b80b3bcfee9540ff1c86aa5faf0001df4f534ac
mfkfawas <mfkfawas@gmail.com>
2025-02-09 07:28:49 +0300

    fix(ui): comment static export config in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
   },
   // our site will kind of get exported completly as static assets that we can deploy anywhere.
   // If any of the route is dynamic, we would get an error while building.
-  output: "export",
+  // output: "export",
 };
 
 export default nextConfig;
