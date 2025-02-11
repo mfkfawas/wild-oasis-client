8fea43cf15cdf2ebf177bc309dca7b67ddf78b52
mfkfawas <mfkfawas@gmail.com>
2025-02-11 11:04:04 +0300

    feat(ui): experimenting with caching and ISR

--- app/cabins/_components/CabinList.tsx
+++ app/cabins/_components/CabinList.tsx
@@ -1,7 +1,12 @@
+import { unstable_noStore as noStore } from "next/cache";
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 
 export async function CabinList() {
+  // Right now, opting out one of the components of page out of the data cache will opt out the entire route out of the data cache.
+  // So now its like revalidating the route, but once partial pre-rendering is stable this will be useful, bcz this component is wrapped using Suspense(so only
+  // this component will become dynamic. So the rest of the page will be static which we call static shell and only this will be in the dynamic hole)
+  noStore();
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
