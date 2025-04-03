50cbfea0914080cf5e33f3b12e03ab68625085a3
mfkfawas <mfkfawas@gmail.com>
2025-04-03 15:13:32 +0300

    feat(auth): getting the user session

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,34 +1,45 @@
-import Link from "next/link";
+import Link from "next/link"
+import { auth } from "../_lib/auth"
 
-export default function Navigation() {
+export default async function Navigation() {
+  //  whenever we do this in a component, it will make the entire route dynamic.
+  //  because this authentication works with cookies and headers. Coz,the auth fn
+  // need to read these cookies from the incoming request.
+  //  Reading cookies will switches the route to dynamic rendering.
+  // Also, this Navigation is part of the whole layout and now all the routes of our app is dynamic.
+  const session = await auth()
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         <li>
-          <Link
-            href="/cabins"
-            className="hover:text-accent-400 transition-colors"
-          >
+          <Link href="/cabins" className="hover:text-accent-400 transition-colors">
             Cabins
           </Link>
         </li>
         <li>
-          <Link
-            href="/about"
-            className="hover:text-accent-400 transition-colors"
-          >
+          <Link href="/about" className="hover:text-accent-400 transition-colors">
             About
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link href="/account" className="hover:text-accent-400 transition-colors flex items-center gap-4">
+              <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link href="/account" className="hover:text-accent-400 transition-colors">
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
-  );
+  )
 }
--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -1,18 +1,19 @@
-import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
-import DateSelector from "./DateSelector";
-import ReservationForm from "./ReservationForm";
+import { auth } from "../_lib/auth"
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service"
+import DateSelector from "./DateSelector"
+import LoginMessage from "./LoginMessage"
+import ReservationForm from "./ReservationForm"
 
-export default async function Reservation({ cabin }) {
-  const [settings, bookingDates] = await Promise.all([
-    getSettings(),
-    getBookedDatesByCabinId(cabin.id)
-  ]);
-
+async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)])
+  const session = await auth()
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-      <DateSelector settings={settings} bookedDates={bookingDates} cabin={cabin} />
-      <ReservationForm cabin={cabin} />
-  </div>
+      <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
+      {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
+    </div>
   )
 }
+
+export default Reservation
