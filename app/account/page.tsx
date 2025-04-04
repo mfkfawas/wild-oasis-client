4a5fe407389b6fbeca51079c19e5b30349ec00bc
mfkfawas <mfkfawas@gmail.com>
2025-04-04 04:32:47 +0300

    feat(auth): server action for SignInButton

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,15 @@
+import { auth } from "../_lib/auth"
+
 export const metadata = {
   title: "Account",
-};
+}
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+
   return (
     <div className="text-red-500">
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Fawaz
-      </h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, {session!.user!.name}</h2>
     </div>
-  );
+  )
 }
