ee84b3a335022d75adf2387892d649df6a02bdd4
mfkfawas <mfkfawas@gmail.com>
2025-04-05 08:19:50 +0300

    feat(reservation): Building the Guest's Reservations Page

--- app/account/reservations/page.tsx
+++ app/account/reservations/page.tsx
@@ -1,18 +1,18 @@
-import ReservationCard from "@/app/_components/ReservationCard";
+import ReservationCard from "@/app/_components/ReservationCard"
+import { auth } from "@/app/_lib/auth"
+import { getBookings } from "@/app/_lib/data-service"
 
 export const metadata = {
   title: "Update profile",
-};
+}
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const session = await auth()
+  const bookings = await getBookings(session!.user!.guestId)
 
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Your reservations
-      </h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">Your reservations</h2>
 
       {bookings.length === 0 ? (
         <p className="text-lg">
@@ -29,5 +29,5 @@ export default function Page() {
         </ul>
       )}
     </div>
-  );
+  )
 }
