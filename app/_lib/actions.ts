278821e4ba621e1cf1b8aa8bb9f196b0f111cd24
mfkfawas <mfkfawas@gmail.com>
2025-04-05 07:43:03 +0300

    feat(profile): update the profile using a server action

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,6 +1,8 @@
 "use server"
 
-import { signIn, signOut } from "./auth"
+import { revalidatePath } from "next/cache"
+import { auth, signIn, signOut } from "./auth"
+import { supabase } from "./supabase"
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" })
@@ -8,4 +10,24 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" })
+}
+
+export async function updateGuest(formData) {
+  const session = await auth()
+
+  // its a common practice to not use try catch, instead throw Error directly, then they will be caught by closest error boundary.
+  if (!session) throw new Error("You must be logged in")
+
+  const nationalID = formData.get("nationalID")
+  const [nationality, countryFlag] = formData.get("nationality").split("%")
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) throw new Error("Please provide a valid national ID")
+
+  const updateData = { nationality, countryFlag, nationalID }
+
+  const { data, error } = await supabase.from("guests").update(updateData).eq("id", session!.user!.guestId)
+
+  if (error) throw new Error("Guest could not be updated")
+
+  revalidatePath("/account/profile")
 }
--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthConfig } from "next-auth"
 import Google from "next-auth/providers/google"
 import { createGuest } from "./data-service"
 import { getGuest } from "./data-service"
 
-const authConfig = {
+const authConfig: NextAuthConfig = {
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
@@ -15,6 +15,7 @@ const authConfig = {
     authorized({ auth, request }) {
       return !!auth?.user
     },
+    // This callback will be called after each signin, as per our bsiness logic, if there is no existing guest, we will create a new guest.
     async signIn({ user, account, profile }) {
       try {
         const existingGuest = await getGuest(user.email)
@@ -26,6 +27,7 @@ const authConfig = {
         return false
       }
     },
+    // This callback will be called by auth.js after each time our session is ready, we r mutating the session and appending the guestId as we need the guestId with session in multiple places in our app.
     async session({ session, user }) {
       const guest = await getGuest(session.user.email)
       session.user.guestId = guest.id
