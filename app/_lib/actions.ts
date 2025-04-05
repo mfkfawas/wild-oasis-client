13d827fb6bf9d30926ec1d3aaf97c7a128238c1b
mfkfawas <mfkfawas@gmail.com>
2025-04-05 13:28:15 +0300

    feat(reservations): Deleting a Reservation

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache"
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase"
+import { getBookings } from "./data-service"
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" })
@@ -31,4 +32,21 @@ export async function updateGuest(formData) {
 
   // NOTE: manual cache revalidation.(This is the second type of revalidation) ---- FYI 1st type is time based that we discussed earlier.
   revalidatePath("/account/profile")
+}
+
+export async function deleteBooking(bookingId) {
+  const session = await auth()
+  if (!session) throw new Error("You must be logged in")
+
+  const guestBookings = await getBookings(session.user.guestId)
+  const guestBookingIds = guestBookings.map((booking) => booking.id)
+
+  // This is a security check for server action - watch lecture - 484. Deleting a Reservation
+  if (!guestBookingIds.includes(bookingId)) throw new Error("You are not allowed to delete this booking")
+
+  const { error } = await supabase.from("bookings").delete().eq("id", bookingId)
+
+  if (error) throw new Error("Booking could not be deleted")
+
+  revalidatePath("/account/reservations")
 }
